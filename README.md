5dc89e22249348d3b2b7843b01e9d647600fb9f6
EdgarG <ed.garciafranco@gmail.com>
2025-05-27 07:46:31 -0500

    Subiendo archivos del proyecto

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # sprint_7_projectf
 Project sprint 7 with data of vehicle sales
+Tenemos un título de Carros 
+Se presenta una impresión de la tabla de datos con la información de las columnas y filas que tiene el dataset
+dos botones: el primero construye un histograma con la información del odómetro; segundo, un gráfico de scatter plot donde podemos ver por año del modelo del carro como es la distribución de los precios.
---
+++ app.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import streamlit as st
+import plotly.express as px
+
+df = pd.read_csv(
+    r'C:\Users\Edgar G\OneDrive\Escritorio\sp7_vehicles_project\sprint_7_projectf\vehicles_us.csv')
+
+st.header('CARROS')
+st.dataframe(df)
+
+button_hist = st.button('Construir Histograma')
+
+if button_hist:
+    st.write('Creación de histograma para el conjunto de datos de anuncio de coches')
+
+    fig = px.histogram(df, x='odometer')  # Crear Histograma
+    st.plotly_chart(fig, use_container_width=True)
+
+button_scatter = st.button('Construir Scatter Plot')
+
+if button_scatter:
+    st.write('Creando Scatter Plot')
+
+    fig1 = px.scatter(df, x='model_year', y='price')
+    st.plotly_chart(fig1, use_container_width=True)
